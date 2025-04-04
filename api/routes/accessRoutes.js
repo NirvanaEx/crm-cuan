788d477cb0f74e8336e93e691d997ced0969d01a
Dias <51851060+NirvanaEx@users.noreply.github.com>
2025-04-04 19:58:29 +0500

    API: Добавлен access для ACCESS и LANGUAGE

--- api/routes/accessRoutes.js
+++ api/routes/accessRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const accessController = require('../controllers/accessController');
 
 // Маршруты для работы с access
-router.get('/', accessController.getAccessList);
-router.get('/:id', accessController.getAccessById);
-router.post('/', accessController.createAccess);
-router.put('/:id', accessController.updateAccess);
-router.delete('/:id', accessController.deleteAccess);
+router.get('/', checkAccess('access_read'), accessController.getAccessList);
+router.get('/:id', checkAccess('access_read'),accessController.getAccessById);
+router.post('/', checkAccess('access_create'), accessController.createAccess);
+router.put('/:id', checkAccess('access_update'), accessController.updateAccess);
+router.delete('/:id', checkAccess('access_delete'), accessController.deleteAccess);
 
 module.exports = router;
