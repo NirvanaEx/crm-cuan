eb4ac75f61d6ffa063a914448f2e1bbd2b58dbfa
Dias <51851060+NirvanaEx@users.noreply.github.com>
2025-04-04 11:14:44 +0500

    FRONTEND: Добавлен Reload для Layout и изменена тема ScrollBar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useReducer, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import Loader from './Loader';
 import { LoadingContext } from '../context/LoadingContext';
 import '../css/Layout.css';
+import { FaRedo } from 'react-icons/fa';
 
 export default function Layout() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const { isLoading } = useContext(LoadingContext);
+    const [refreshKey, forceUpdate] = useReducer(x => x + 1, 0);
 
     return (
         <div className="layout-container">
@@ -16,8 +18,15 @@ export default function Layout() {
             <div className="layout-main">
                 <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
                 <div className="layout-content" style={{ position: 'relative' }}>
-                    <Outlet />
+                    <Outlet key={refreshKey} />
                     {isLoading && <Loader text="Загружаем данные..." />}
+                    <button
+                        onClick={forceUpdate}
+                        className="reload-button"
+                        aria-label="Обновить Layout"
+                    >
+                        <FaRedo size={20} />
+                    </button>
                 </div>
             </div>
         </div>
